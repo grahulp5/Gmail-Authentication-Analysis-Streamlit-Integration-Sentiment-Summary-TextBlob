8c210a4f51ab87b54a48322bd2e45767103c6065
Rahul Gupta <grahulp5@gmail.com>
2024-08-11 05:17:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as stc
 from RetrieveSpam import analysis
 from SentimentAnalysis import sentanalysis
+pip install openai
 
 # HTML template for the header
 html_temp = """
