9f4444ddf45d7f6122b0726ac1e15621b16f4d24
Rahul Gupta <grahulp5@gmail.com>
2024-08-11 20:13:20 -0700

    Update RetrieveSpam.py

--- RetrieveSpam.py
+++ RetrieveSpam.py
@@ -1,6 +1,7 @@
 import os.path
 import pickle
 import base64
+import json
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
